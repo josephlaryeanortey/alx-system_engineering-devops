728068c916f0007334871ca31d46a532cf89bd3c
josephlaryeanortey <christiangospelhype@gmail.com>
2024-08-13 07:54:58 -0700

    apiiadvance

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python3
-"""Function to query subscribers on a given Reddit subreddit."""
+"""Script that returns the number of subscribers of a subreddit"""
 import requests
 
-
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
+    """Function that returns the number of subscribers of a subreddit"""
+    if not isinstance(subreddit, str):
         return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
+
+    headers = {'User-Agent': 'selBot/1.0'}
+    URL = f'https://www.reddit.com/r/{subreddit}/about.json'
+
+    try:
+        response = requests.get(URL, headers=headers, allow_redirects=False)
+        response.raise_for_status()  # Raises HTTPError for bad responses
+        data = response.json()
+        return data.get('data', {}).get('subscribers', 0)  # Safely access 'subscribers'
+
+    except requests.exceptions.RequestException:
+        return 0

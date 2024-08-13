2f7f2a5e32b668de8fa4870c506d92db22281848
josephlaryeanortey <christiangospelhype@gmail.com>
2024-08-13 08:27:25 -0700

    apiadvance

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,21 +1,33 @@
 #!/usr/bin/python3
-"""Function to print hot posts on a given Reddit subreddit."""
+"""Script that returns top 10 hot posts of a subreddit"""
 import requests
 
 
 def top_ten(subreddit):
-    """Print the titles of the 10 hottest posts on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    params = {
-        "limit": 10
-    }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-    if response.status_code == 404:
-        print("None")
+    """Function that prints top 10 posts of a subreddit"""
+    if subreddit is None or not isinstance(subreddit, str):
+        print(None)
         return
-    results = response.json().get("data")
-    [print(c.get("data").get("title")) for c in results.get("children")]
+
+    headers = {'User-Agent': 'selBot/2.0'}
+    URL = f'https://www.reddit.com/r/{subreddit}/hot.json?limit=10'
+
+    try:
+        response = requests.get(URL, headers=headers, allow_redirects=False)
+        response.raise_for_status()
+        data = response.json()
+        posts = data['data']['children']
+
+        if not posts:
+            print("None")
+
+        else:
+            for post in posts:
+                title = post['data']['title']
+                print(title)
+
+    except requests.exceptions.RequestException:
+        print(None)
+
+    except (KeyError, ValueError):
+        print(None)
